c833f26f2515599cd045005ac011c7353d17a4af
Maria-Bianca Cindroi <bianca.cindroi@gmail.com>
2017-01-14 18:55:19 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -6,7 +6,6 @@ import library.MainFrame;
 /** Class representing the runnable for the bookStore
  * 
  * @author Maria-Bianca Cindroi
- * refactored from Team2: Project Programming, Wine Advisor
  *
  */
 public class Main {
